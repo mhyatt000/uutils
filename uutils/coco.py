32b5f3274bf9d1a3a2438570b7a89db1ca5e89c1
Matthew Hyatt <mhyatt000@gmail.com>
2022-06-24 15:11:47 -0400

    deploy

--- uutils/coco.py
+++ uutils/coco.py
@@ -2,15 +2,7 @@ from pprint import pprint
 from pycocotools.coco import COCO
 
 
-<<<<<<< HEAD
 def id2l(coco):
-=======
-def id2l(root):
-
-    dset = "val"
-    coco_path = f"{root}/COCOdataset2017"
-    coco = COCO(f"{coco_path}/annotations/instances_val2017.json")
->>>>>>> 17670707f224613b6a2f604dd1cf533f9e32a559
 
     catIds = coco.getCatIds()
     cats = coco.loadCats(catIds)
