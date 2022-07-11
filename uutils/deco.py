773bb3dc9c3d13cd4f880ca13a6020d8f9c259ca
Matthew Hyatt <mhyatt000@gmail.com>
2022-07-11 19:05:07 -0400

    deploy

--- uutils/deco.py
+++ uutils/deco.py
@@ -1,3 +1,4 @@
+import time
 import functools
 import sys
 
@@ -17,7 +18,7 @@ def stdout(file):
     return deco
 
 
-def repeat(n):
+def repeat(func=None, *, n=2):
     def deco(func):
         @functools.wraps(func)
         def wrap(*args, **kwargs):
@@ -29,20 +30,85 @@ def repeat(n):
 
         return wrap
 
-    return deco
+    return deco if func is None else deco(func)
 
-def timer():
+
+def timer(func=None):
+    @functools.wraps(func)
+    def wrap(*args, **kwargs):
+
+        tic = time.perf_counter()
+        result = func(*args, **kwargs)
+        toc = time.perf_counter()
+        print(f"Done (t={round(toc-tic,2)})s")
+        return result
+
+    return wrap
+
+
+class DecoratorClass:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        print("decorated class")
+        return self.func(*args, **kwargs)
+
+
+def singleton(cls):
+    """Make a class a Singleton class (only one instance)"""
+
+    @functools.wraps(cls)
+    def wrap(*args, **kwargs):
+        if not wrap.instance:
+            wrap.instance = cls(*args, **kwargs)
+        return wrap.instance
+
+    wrap.instance = None
+    return wrap
+
+
+@singleton
+class TheOne:
+    "example"
+    pass
+
+
+def sleep(func=None, *, time=1):
+    """Sleep given amount of seconds before calling the function"""
+
     def deco(func):
         @functools.wraps(func)
-        def weap(*args, **kwargs):
+        def wrap(*args, **kwargs):
 
-            tic = time.perf_timer()
-            result = func(*args, **kwargs)
-            toc = time.per_timer()
-
-            print(f'Done (t={round(toc-tic,2)}s)')
-            return result
+            time.sleep(time)
+            return func(*args, **kwargs)
 
         return wrap
-    return deco
+
+    return deco if func is None else deco(func)
+
+
+def cache(func):
+    """Keep a cache of previous function calls"""
+    '''use @functools.lru_cache'''
+
+    @functools.wraps(func)
+    def wrap(*args, **kwargs):
+
+        try:
+            return wrap.cache[args, kwargs]
+        except:
+            wrap.cache[args, kwargs] = func(*args, **kwargs)
+            return wrap.cache[args, kwargs]
+
+    wrap.cache = dict()
+    return wrap
+
+PLUGINS = dict()
+def register(func):
+    """Register a function as a plug-in"""
+
+    PLUGINS[func.__name__] = func
+    return func
 
